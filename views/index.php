3a32e245a7b722f1e8fedfc46015e37d5be73775
Iaroslav Krasilchuk <legend.ko@hotmail.com>
2018-01-24 13:48:12 +0200

    Update index.php

--- views/index.php
+++ views/index.php
@@ -5,7 +5,6 @@
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
 	<link rel="stylesheet" href="/css/styles.css">
 
 </head>
